a1473eedd68bef0119e899e48f91168faaa9f80c
myselfcarvajal <myselfcarvajal@gmail.com>
2026-04-03 21:05:27 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
+from src.posts.schemas import PostCreate
+from database import create_db_and_tables, get_async_session, Post
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy import select
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(title="Media Sharing API", lifespan=lifespan)
+
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url="https://example.com/image.jpg",
+        file_type="photo",
+        file_name="dummy name",
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    post_data = []
+    for post in posts:
+        post_data.append({
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at
+        })
+
+    return post_data

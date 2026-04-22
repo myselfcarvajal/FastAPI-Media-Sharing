37871dfeda3b6a8652741f1d77072120dc5c9a54
myselfcarvajal <myselfcarvajal@gmail.com>
2026-04-22 14:10:18 -0500

    feat: set up Docker environment with Dockerfile, docker-compose

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 import uvicorn
 
+load_dotenv()
+
+PORT = int(os.environ["PORT"])
+
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=PORT, reload=True)
